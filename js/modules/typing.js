823ba3132d8d0810b2d4af66b94b6552b1d3afb9
Gabriela Souza <gabrielaguaru@yahoo.com.br>
2022-04-10 17:31:41 -0300

    acrescentando demais projetos

--- js/modules/typing.js
+++ js/modules/typing.js
@@ -1,11 +1,11 @@
 export default function initTyping() {
-    let i = 0;
+    let letter = 0;
     const text = '     Gabriela Souza';
 
     function typeWriter() {
-        if(i < text.length) {
-            document.querySelector("#sentence").innerHTML += text.charAt(i);
-            i++;
+        if(letter < text.length) {
+            document.querySelector(".principal__sentence").innerHTML += text.charAt(letter);
+            letter++;
             setTimeout(typeWriter, 180)
         }
     }
